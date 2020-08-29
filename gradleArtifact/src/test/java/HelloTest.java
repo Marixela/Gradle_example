76aad039ee6e312531c0101a5ae31f3b0f0c3975
Maricela Romero <maryxelaromer@gmail>
2020-08-29 12:40:03 -0500

    Example Gradle

---
+++ gradleArtifact/src/test/java/HelloTest.java
@@ -0,0 +1,21 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class HelloTest {
+    @Before
+    public void beforeTestDo(){
+        System.out.println("Antes del Test");
+    }
+    @Test
+    public void testGetHello(){
+        System.out.println("Durante Test");
+        assertEquals("Hola mundito", new Hello().getHello());
+    }
+    @After
+    public void afterTestDo(){
+        System.out.println("Despues del test");
+    }
+}
